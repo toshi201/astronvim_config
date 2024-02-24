6e835838474401253142a115e664958fe4d33560
toshi201 <toshi.stce@gmail.com>
2024-02-24 22:49:14 +0900

    emacs key binding

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,14 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- ["j"] = { "gj" }
+  },
+  i = {
+    -- emacs like key mapping
+    ["<C-f>"] = { "<Right>" },
+    ["<C-b>"] = { "<Left>" },
+    ["<C-a>"] = { "<Home>" },
+    ["<C-e>"] = { "<End>" },
   },
   t = {
     -- setting a mapping to false will disable it
