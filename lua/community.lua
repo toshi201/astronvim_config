8e7f24fc877d3a2fe7cd4ba7a50f529dad46a1ff
toshi201 <toshi.stce@gmail.com>
2024-04-10 23:36:58 +0900

    add plugin

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,11 @@ return {
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.haskell" },
   { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.recipes.heirline-clock-statusline" },
+  { import = "astrocommunity.recipes.vscode-icons" },
   -- import/override with your plugins folder
 }
