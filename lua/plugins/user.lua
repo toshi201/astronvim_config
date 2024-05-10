2b6e4b7fdd8b9bcdd69d34984f95c9cbd09d8cca
toshi201 <toshi.stce@gmail.com>
2024-05-10 22:38:01 +0900

    rem unneeded luasnip config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -55,16 +55,6 @@ return {
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-
-  {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
